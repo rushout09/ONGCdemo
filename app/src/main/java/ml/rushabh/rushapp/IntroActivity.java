dae1d03f7ee4ddb959a1e0fc3511e819827f291a
Rushabh <rushabh@hpnotebook.ib-wrb304n.setup.in>
2018-06-24 23:16:42 +0530

    Initial commit

---
+++ app/src/main/java/ml/rushabh/rushapp/IntroActivity.java
@@ -0,0 +1,63 @@
+package ml.rushabh.rushapp;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class IntroActivity extends AppCompatActivity {
+    private Button startButton;
+    private static final int RC_SIGN_IN = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+
+        startButton = (Button)findViewById(R.id.start_button);
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                // Choose authentication providers
+                List<AuthUI.IdpConfig> providers = Arrays.asList(
+                        new AuthUI.IdpConfig.EmailBuilder().build(),
+                        new AuthUI.IdpConfig.GoogleBuilder().build());
+                startActivityForResult(AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setAvailableProviders(providers)
+                        .build(),RC_SIGN_IN);
+
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RC_SIGN_IN) {
+            IdpResponse response = IdpResponse.fromResultIntent(data);
+
+            if (resultCode == RESULT_OK) {
+                // Successfully signed in
+                Intent intent = new Intent(this, MainActivity.class);
+                startActivity(intent);
+                finish();
+
+            } else {
+                Toast.makeText(this,"Sign In Failed, Try Again!",Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+}
---
+++ app/src/main/java/ml/rushabh/rushapp/MainActivity.java
@@ -0,0 +1,68 @@
+package ml.rushabh.rushapp;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import com.firebase.ui.auth.AuthUI;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView email_tv;
+    TextView name_tv;
+    private FirebaseUser mUser;
+    private FirebaseAuth mAuth;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        email_tv = (TextView)findViewById(R.id.user_email);
+        name_tv = (TextView)findViewById(R.id.user_name);
+        mUser = mAuth.getInstance().getCurrentUser();
+        if (mUser != null) {
+            // User is signed in
+            email_tv.setText(mUser.getEmail());
+            name_tv.setText(mUser.getDisplayName());
+        } else {
+            // No user is signed in
+            email_tv.setText("No user is signed in");
+        }
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.main,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(R.id.sign_out == item.getItemId()){
+            AuthUI.getInstance()
+                    .signOut(this);
+            Intent intent = new Intent(MainActivity.this,IntroActivity.class);
+            startActivity(intent);
+            finish();
+
+        }
+        return true;
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        AuthUI.getInstance()
+                .signOut(this);
+    }
+}
